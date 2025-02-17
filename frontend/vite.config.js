4e2e9eb64193a3ce8a9a7d8e64dea245bcb0cf40
md-arif-islam <arifislamarif344@gmail.com>
2025-02-18 05:21:40 +0600

    Deploy skeleton update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    // base: "/dist/",
+    base: "/dist/",
     plugins: [react()],
-    // server: {
-    //     host: true,
-    // },
-    // build: {
-    //     outDir: "../public/dist",
-    //     emptyOutDir: true,
-    // },
+    server: {
+        host: true,
+    },
+    build: {
+        outDir: "../public/dist",
+        emptyOutDir: true,
+    },
 });
