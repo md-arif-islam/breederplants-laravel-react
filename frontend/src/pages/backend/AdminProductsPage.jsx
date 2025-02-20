971fb5b007e3d6b3b09dab7fb1879c370452f45a
md-arif-islam <arifislamarif344@gmail.com>
2025-02-20 22:07:23 +0600

    v2.2 deploy

--- frontend/src/pages/backend/AdminProductsPage.jsx
+++ frontend/src/pages/backend/AdminProductsPage.jsx
@@ -92,7 +92,7 @@ export default function AdminProductsPage() {
                                                 <tr>
                                                     <td
                                                         colSpan={7}
-                                                        className="text-center py-4"
+                                                        className="text-center py-4 bg-red-100 p-5 rounded-md"
                                                     >
                                                         No products found.
                                                     </td>
