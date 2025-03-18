5671682dc22f2958b34b29899e44914e4070365b
md-arif-islam <arifislamarif344@gmail.com>
2025-03-19 03:36:04 +0600

    Add Tailwind CSS typography plugin and format tailwind.config.js for consistency

--- frontend-app/tailwind.config.js
+++ frontend-app/tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        inter: ['Inter', 'sans-serif'],
-      },
-      colors: {
-        primary: '#45A049',
-        secondary: '#2a2a2a',
-      },
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+                inter: ["Inter", "sans-serif"],
+            },
+            colors: {
+                primary: "#45A049",
+                secondary: "#2a2a2a",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        require("@tailwindcss/typography"),
+        // Add other plugins here if needed
+    ],
 };
